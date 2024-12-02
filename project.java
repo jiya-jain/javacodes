c81305c256dc76eaa90e0147de9510c8054bcd91
Jiya Jain <jiya.jain479@gmail.com>
2024-12-02 21:18:48 +0530

    project

---
+++ project.java
@@ -0,0 +1,11 @@
+public class project {
+    public static void main(String[] args) {
+        System.out.println("hey");
+    }
+    
+}
+class main{
+    public static void main (String args[]){
+    System.out.println("jiya jain");
+}2
+}
